39db2e3328d371add9ff33e399697c1c3b72b7bc
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-03-24 11:00:03 -0400

    Add files via upload

---
+++ Ch 13 Questions/settings13.py
@@ -0,0 +1,11 @@
+
+class Settings:
+    """A class to store settings for Star Invasion. """
+    
+    def __init__(self):
+        """Initialize the game's setting. """
+        # Screen settings
+        self.screen_width = 500
+        self.screen_height = 500
+        self.bg_color = (13, 25, 255)
+        
