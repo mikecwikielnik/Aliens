29c6471c0d26185c797f1bc7d0b70a21b6d51068
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-03-26 17:05:20 -0400

    Add files via upload

--- alien_invasion.py
+++ alien_invasion.py
@@ -88,7 +88,11 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
             
     def _update_aliens(self):
-        """update the positions of all aliens in the fleet. """
+        """
+        Check if the fleet is at an edge, 
+            then update the position of all aliens in the fleet.
+        """
+        self._check_fleet_edges()
         self.aliens.update()
         
     def _update_screen(self):
@@ -130,10 +134,19 @@ class AlienInvasion:
         alien.rect.y = alien_height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
         
-    def _check_fleet_edge(self):
-        
-        
-                
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge. """
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+            break
+    
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction. """
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+         
             
 if __name__ == '__main__':
         # Make a game instance, and run the game.
