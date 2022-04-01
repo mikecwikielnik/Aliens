53e33a62acef8bdd5ead3f2a6d53d9b6da532afb
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-04-01 12:10:45 -0400

    Add files via upload

--- alien_invasion.py
+++ alien_invasion.py
@@ -71,8 +71,22 @@ class AlienInvasion:
                 
     def _check_play_button(self, mouse_pos):
         """Start a new game when the player clicks Play. """
-        if self.play_button.rect.collidepoint(mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
+            # Reset the game statistics.
+            self.stats.reset_stats()
             self.stats.game_active = True
+            
+            # Get rid of any remaining aliens and bullets.
+            self.aliens.empty()
+            self.bullets.empty()
+            
+            # Create a new fleet and center the ship.
+            self._create_fleet()
+            self.ship.center_ship()
+            
+            # Hide the mouse cursor.
+            pygame.mouse.set_visible(False)
                             
     def _check_keydown_events(self, event):
         """Respond to keypresses. """
@@ -154,8 +168,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.stats.game_active = False
-        
-        
+            pygame.mouse.set_visible(True)        
         
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen. """
